b651e4ae973c6c18de3b40ddec490956bcf69e62
dmhoang <dmhoang08t1@gmail.com>
2020-01-08 14:32:23 +0700

    first program

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # golang-01-2020
 The language Golang
+
+* Run a golang file
+  > go run fileName.go
--- hello.go
+++ hello.go
@@ -4,31 +4,6 @@ import (
 	"fmt"
 )
 
-var i int = 2
-
-func add() int {
-	sum := 0
-	for i := 0; i < 4; i++ {
-		sum += i
-	}
-	return sum
-}
-
-func switchFun(i int) int {
-	switch i {
-	case 1:
-		fmt.Println("one")
-	case 2:
-		fmt.Println("two")
-	default:
-		fmt.Println("zero")
-	}
-	return i
-}
-
 func main() {
-	fmt.Println("result is", add())
-	fmt.Println("i", i)
-	fmt.Println("switch is", switchFun(1))
-	fmt.Printf("Type: %T Value: %v\n", i, i)
+	fmt.Println("Hello Golang!")
 }
